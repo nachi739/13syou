33bdf8e9b5e1ab6638071509a63d5faf4bd8c448
nachi739 <namiki081025@gmail.com>
2021-09-30 16:51:24 +0900

    code13ex_04

---
+++ 13章/src/code13ex_04/Main.java
@@ -0,0 +1,24 @@
+package code13ex_04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		Wizard w = new Wizard();
+		Wand ww = new Wand();
+
+		h.setName("勇者");
+		h.setHp(100);
+
+		w.setName("魔法使い");
+		w.setHp(50);
+		w.setMp(200);
+		w.setWand(ww);
+
+
+		ww.setName("魔法の杖");
+		ww.setPower(50);
+		w.heal(h);
+	}
+
+}
---
+++ 13章/src/code13ex_04/Sword.java
@@ -0,0 +1,6 @@
+package code13ex_04;
+
+public class Sword {
+	String name;
+	int damage;
+}
