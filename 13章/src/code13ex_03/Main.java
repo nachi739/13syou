1949c759b42633fa9840c68bf913925f3f394ef4
nachi739 <namiki081025@gmail.com>
2021-09-30 16:33:21 +0900

    code13ex_03

--- 13章/src/code13ex_03/Main.java
+++ 13章/src/code13ex_03/Main.java
@@ -4,17 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Hero h = new Hero();
+		Wizard w = new Wizard();
+		Wand ww = new Wand();
+
 		h.setName("勇者");
 		h.setHp(100);
 
-		Wizard w = new Wizard();
 		w.setName("魔法使い");
 		w.setHp(50);
 		w.setMp(200);
+		w.setWand(ww);
 
 
-
-		Wand ww = new Wand();
 		ww.setName("魔法の杖");
 		ww.setPower(50);
 		w.heal(h);
